fe51f2104bfe2dc2c577035177749221e35972dd
spacer131 <flochin.eric@neuf.fr>
2022-12-30 16:53:46 +0100

    blog1

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\CallbackTransformer;
 
 class UserType extends AbstractType
 {
@@ -15,7 +16,13 @@ class UserType extends AbstractType
             ->add('email')
             ->add('roles')
             ->add('password')
+            
         ;
+        $builder->get('roles')
+        ->addModelTransformer(new CallbackTransformer(
+            fn ($rolesAsArray) => count($rolesAsArray) ? $rolesAsArray[0] : null,
+            fn ($rolesAsString) => [$rolesAsString]
+        ));
     }
 
     public function configureOptions(OptionsResolver $resolver): void
